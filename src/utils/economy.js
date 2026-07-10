537ca145802a62e8bef097e8ce31018e275974f5
MinoxJJS <asakc@icloud.com>
2026-07-09 23:01:44 -0400

    Update economy.js

--- src/utils/economy.js
+++ src/utils/economy.js
@@ -8,8 +8,8 @@ import { validateDiscordId, validateNumber } from './validation.js';
 import { DEFAULT_ECONOMY_DATA } from './constants.js';
 
 const ECONOMY_CONFIG = BotConfig.economy || {};
-const BASE_BANK_CAPACITY = ECONOMY_CONFIG.baseBankCapacity || 10000;
-const BANK_CAPACITY_PER_LEVEL = ECONOMY_CONFIG.bankCapacityPerLevel || 5000;
+const BASE_BANK_CAPACITY = ECONOMY_CONFIG.baseBankCapacity || 10000000000;
+const BANK_CAPACITY_PER_LEVEL = ECONOMY_CONFIG.bankCapacityPerLevel || 5000000000;
 const DAILY_AMOUNT = ECONOMY_CONFIG.dailyAmount || 100;
 const WORK_MIN = ECONOMY_CONFIG.workMin || 10;
 const WORK_MAX = ECONOMY_CONFIG.workMax || 100;
