54245c3ed37c51020a62756e7d9749e8504b13f6
MinoxJJS <asakc@icloud.com>
2026-07-09 23:47:28 -0400

    Change gamble cooldown duration
    Reduced the gamble cooldown from 2 minutes to 0.001 minutes.

--- src/commands/Economy/gamble.js
+++ src/commands/Economy/gamble.js
@@ -8,7 +8,7 @@ const BASE_WIN_CHANCE = 0.4;
 const CLOVER_WIN_BONUS = .5;
 const CHARM_WIN_BONUS = 0.08;
 const PAYOUT_MULTIPLIER = 3.0;
-const GAMBLE_COOLDOWN = 2 * 60 * 1000;
+const GAMBLE_COOLDOWN = .001 * 60 * 1000;
 
 export default {
     data: new SlashCommandBuilder()
